59f92dc7f9e28eac927a0f652652ec6faa7b8414
Michael Ambrus <michael.ambrus@sonymobile.com>
2015-03-04 14:13:57 +0100

    Added libmtime from project sampler.

---
+++ initfini.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <config.h>
+#include <sysmtime.h>
+#define __init __attribute__((constructor))
+#define __fini __attribute__((destructor))
+
+/* Module initializers/deinitializers. When used as library (who don't have
+ * a natural entry/exit function) these are used to initialize
+ * deinitialize. Use to set predefined/default states and cleanup.
+ *
+ * This will work with shared libraries as well as with static as they get
+ * invoked by RTL load/unload, with or without C++ code (i.e. functions will
+ * play nice with C++ normal ctors/dtors).
+ *
+ * Keep mtime in to at least once per new build-/run-environment assert that
+ * the mechanism works.
+ */
+
+void __init __mtime_init(void)
+{
+}
+
+void __fini __mtime_fini(void)
+{
+}
